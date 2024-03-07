4c9e6df7c90bc32629f24a22738550c1413eb513
Roman Kharitonov <rkharito@yandex.ru>
2024-03-07 23:50:44 +1000

    Squash same resistance values into a single percentage string

--- Presentation/Drawers/ResistancesDrawer.lua
+++ Presentation/Drawers/ResistancesDrawer.lua
@@ -19,30 +19,55 @@ ID_TO_DISPLAY["shadow"] = { label = "Shadow", color = "|cffcd81dc" }
 ---@param resistances ResistanceVO[]
 ---@param tooltip TooltipInterface
 function ResistancesDrawer:Draw(resistances, tooltip)
-    ---@type table<string, ResistanceVO>
-    local label_to_vo = {}
     ---@type string[]
     local sorted_labels = {}
+    ---@type table<string, ResistanceId>
+    local label_to_id = {}
     for _, vo in ipairs(resistances) do
         local display = ID_TO_DISPLAY[vo:GetId()]
-        label_to_vo[display.label] = vo
         tinsert(sorted_labels, display.label)
+        label_to_id[display.label] = vo:GetId()
     end
     sort(sorted_labels)
 
-    ---@type string[]
-    local value_strings = {}
-    for _, label in ipairs(sorted_labels) do
-        local vo = label_to_vo[label]
-        local display = ID_TO_DISPLAY[vo:GetId()]
+    ---@type table<string, number>
+    local id_to_value = {}
+    local has_positive_values = false
+    local has_different_values = false
+    local previous_value
+    for _, vo in ipairs(resistances) do
         local value = round(vo:GetAverageResistanceInPercents(), 0)
+
         if value > 0 then
-            tinsert(value_strings, paint(format("%s %d%%", label, value), display.color))
+            has_positive_values = true
         end
+
+        if previous_value == nil then
+            previous_value = value
+        end
+        if value ~= previous_value then
+            has_different_values = true
+        end
+        previous_value = value
+
+        id_to_value[vo:GetId()] = value
     end
 
-    if getn(value_strings) == 0 then
+    ---@type string[]
+    local value_strings = {}
+    if not has_positive_values then
         tinsert(value_strings, "None")
+    elseif not has_different_values then
+        tinsert(value_strings, format("All %d%%", previous_value))
+    else
+        for _, label in ipairs(sorted_labels) do
+            local id = label_to_id[label]
+            local display = ID_TO_DISPLAY[id]
+            local value = id_to_value[id]
+            if value > 0 then
+                tinsert(value_strings, paint(format("%s %d%%", label, value), display.color))
+            end
+        end
     end
 
     tooltip:AddValue("Resistances", strjoin(value_strings, ", "), true)
