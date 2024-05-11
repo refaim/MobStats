b759a568b3cc928cd1cda1fec2301d7182b962ec
Roman Kharitonov <rkharito@yandex.ru>
2024-05-11 16:53:33 +1000

    Display zero armor as "None" (closes #7)

--- Presentation/Drawers/ArmorDrawer.lua
+++ Presentation/Drawers/ArmorDrawer.lua
@@ -6,13 +6,14 @@ ArmorDrawer = {}
 ---@param value ArmorVO
 ---@param tooltip TooltipInterface
 function ArmorDrawer:Draw(value, tooltip)
-    local amount = round(value:GetAmount(), 0)
-    if amount == 0 then
-        return
+    local integer_amount = round(value:GetAmount(), 0)
+
+    local amount_string
+    if integer_amount == 0 then
+        amount_string = "None"
+    else
+        amount_string = format("%d (%d%% DR)", integer_amount, round(value:GetDamageReductionInPercents(), 0))
     end
 
-    tooltip:AddValue(
-        "Armor",
-        format("%s (%s%% DR)", amount, round(value:GetDamageReductionInPercents(), 0)),
-        false)
+    tooltip:AddValue("Armor", amount_string, false)
 end
